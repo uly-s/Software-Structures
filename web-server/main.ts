99e1d503d28450d4b660ea817f43c6f3d54b7249
uly-s <grant@uly-s.com>
2024-06-02 20:03:28 -0500

    made it almost through chapter 3

--- web-server/main.ts
+++ web-server/main.ts
@@ -1,2 +1,30 @@
-// index.ts
-console.log("hello world");
+import * as net from "net";
+
+let server = net.createServer();
+
+function connect(socket: net.Socket): void {
+    console.log("Connected: ", socket.remoteAddress, socket.remotePort);
+
+    socket.on('end', () => {
+        // FIN received. The connection will be closed automatically.
+        console.log('EOF.');
+    });
+
+    socket.on('data', (data: Buffer) => {
+        console.log('data:', data);
+        socket.write(data); // echo back the data.
+
+        // actively closed the connection if the data contains 'q'
+        if (data.includes('q')) {
+            console.log('closing.');
+            socket.end();   // this will send FIN and close the connection.
+        }
+    });
+}
+
+server.on('connection', connect);
+server.on('error', (err: Error) => { throw err; });
+
+server.listen({host: '127.0.0.1', port: 1234});
+
+console.log(server);
